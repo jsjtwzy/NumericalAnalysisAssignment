b831ea09bdd4bb1c08d627559f811d121d44a15f
ZiyuanW <jsjtwzy@163.com>
2023-01-19 21:04:50 +0800

    Numerical Analysis v1

---
+++ vector.py
@@ -0,0 +1,16 @@
+import itertools
+class Vector:
+    def __init__(self, *components):
+        dim = len(components)
+        self._dim = dim
+        self._components = components
+    @property
+    def dim(self):
+        return self._dim
+    def __iter__(self):
+        return iter(self._components)
+    def __add__(self, other):
+        pairs = itertools.zip_longest(self, other, fillvalue=0.0)
+        return 
+y = Vector(1,2)
+print(y.dim)
