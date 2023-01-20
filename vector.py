c032a6317d83815ad60b8e893b7b9570d47a3600
ZiyuanW <jsjtwzy@163.com>
2023-01-20 12:55:38 +0800

    Numerical Analysis v1

--- vector.py
+++ vector.py
@@ -1,6 +1,14 @@
 import itertools
 import numbers
 import math
+
+def epsilon(i,j,k):
+    if (j -i) *(k -j) *(i -k) == -2:
+        return 1
+    elif (j -i) *(k -j) *(i -k) == 2:
+        return -1
+    else:
+        return 0
 class Vector:
     def __init__(self, *components):
         dim = len(components)
@@ -12,6 +20,8 @@ class Vector:
         return self._dim
     def __iter__(self):
         return iter(self._components)
+    def __getitem__(self, index):
+        return self._components[index]
     def __repr__(self):
         return 'Vector' +str(self._components)
     
@@ -44,6 +54,20 @@ class Vector:
         return sum([a *b for a, b in pairs])
     def __rmatmul__(self, other):
         return self @other
+    @classmethod
+    def crossmul(cls, self, other):
+        if self.dim <=3 and other.dim <=3:
+            selfIn = self +Vector(0, 0, 0)
+            otherIn = other +Vector(0, 0, 0)
+            y = [0, 0, 0]
+            for i in range(3):
+                for j in range(3):
+                    for k in range(3):
+                        y[i] += epsilon(i, j, k) *selfIn[j] *otherIn[k]
+            print(y)
+            return Vector(*y)
+        else:
+            return NotImplemented
 
-y = abs(-Vector(1, 2)) *Vector(1, 3)
+y = Vector.crossmul(-Vector(1, 2), Vector(1, 3))
 print(y)
