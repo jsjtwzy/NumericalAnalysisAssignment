5a80f770dc3cbbff4e839439c6ff39f02b18cdc4
ZiyuanW <jsjtwzy@163.com>
2023-02-08 19:27:13 +0800

    NA

--- Ex5Fit3.py
+++ Ex5Fit3.py
@@ -3,14 +3,9 @@ import sympy as sp
 
 from Ex5Fit import Fit
 
-rho = 1
 n = 3
-para = ', '.join(['a{}'.format(i) for i in range(n+1)])
-symbols = 'x, ' +para
-x, *a = sp.symbols(symbols)
-
-xserial = np.array((0,0.1,0.2,0.3,0.5,0.8,1))
-yserial = np.array((1,0.41,0.50,0.61,0.91,2.02,2.46))
+symbols = ', '.join(['a{}'.format(i) for i in range(n+1)])
+a = sp.symbols(symbols)
 
 def cal():
     return str(Fit(a, n))
