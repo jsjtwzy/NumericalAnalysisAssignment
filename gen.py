e0be8aa8348b14684cffab2239169eb27783be66
ZiyuanW <jsjtwzy@outlook.com>
2023-02-15 10:12:36 +0800

    NA

--- gen.py
+++ gen.py
@@ -1,5 +1,31 @@
-a = range(40)
-def gen():
-    res = yield from a
-    return res
-print(sum(gen()))
+import collections
+
+Result = collections.namedtuple('Result', 'count average')
+
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield
+        if term == None:
+            break
+        total += term
+        count += 1
+        average = total/count
+    return Result(count, average)
+
+def grouper(results, key):
+    while True:
+        results[key] = yield from averager()
+
+def main(data):
+    results = []
+    for key, values in data.items():
+        group = grouper(results, key)
+        next(group)
+        for value in values:
+            group.send(value)
+        group.send(None)
+    
+    print(results)
